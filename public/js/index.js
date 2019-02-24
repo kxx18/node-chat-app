a610298806d2790ed86e46809cf5ff25822b0f25
kxh <kxh1995@gmail.com>
2019-02-24 22:49:04 +0100

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+//call IO is a method that is available to us because of socket.io library
+//here we will make a request to the server to open a connection
+var socket =io();
+
+socket.on('connect', function(){
+    console.log('Conncected to server');
+
+    socket.emit('createMessage', {
+        from: 'Andrew',
+        text: 'Yup, that works for me.'
+    })
+});
+
+socket.on('disconnect', function(){
+    console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function(email){
+    console.log('New email', email);
+});
+
+socket.on('newMessage', function(message){
+    console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -1,14 +1,40 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 //middleware to serve publicPath
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
+//lets you register an event listener,, listen for a new connection
+///you can listen for a new conncetion and do somthing when that connection comes in
+//provide a callback function to hande the connectio
+io.on('connection', function(socket){
+    console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'mike@example.com',
+        text: 'Hey, hey from server',
+        createAt: 123
+
+    });
+    
+    socket.on('createMessage', (message)=>{
+        console.log('createMessage', message);
+    });
+
+    socket.on('disconnect', ()=>{
+        console.log('User was disconnected');
+    })
+});
+
+server.listen(3000, ()=>{
     console.log(`Server is up on port ${port}`);
-})
+});
 
 
